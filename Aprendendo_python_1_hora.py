53ce06ba5330f9dc27c5c950aa15e1baacac0384
root <root@teamsbot.181.213.132.2>
2025-01-17 12:29:41 +0000

    pomodore 1

---
+++ Aprendendo_python_1_hora.py
@@ -0,0 +1,52 @@
+type(1)
+print(type(1))
+print(43%6)
+
+print(43/6)
+
+print(43//6)
+
+print(type(7.16))
+
+pi = 3.1415
+raio = 1
+print(pi * (raio ** 2))
+
+
+print("Olá Mundo")
+
+print(type('Renzo'), type("Renzo"), type("""Renzo"""), type('''Renzo'''))
+
+nome = 'Renzo \n Nuccitelli'
+print(nome)
+
+
+nome = '''Renzo
+    Nuccitelli'''
+
+print(nome)
+
+
+print(type(True), type(False))
+
+print(not(True))
+
+print(not(False))
+
+print(True and False)
+print(True and True)
+print(False and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+print(5 < 6)
+
+print(5 <= 6)
+
+print(5 == 6)
+
+print(5 > 6)
+
+print(5 != 6)
