bf5734898bddac24e7d0f7eb34b2842c27795bba
Marcos Antonio Urbanski <35203750+urbanskii@users.noreply.github.com>
2025-01-17 10:43:19 -0300

    Pomodoro final (#39)
    * pomodore 1
    
    * testando o commit
    
    * TEstando
    
    * Pomodoro 2
    
    * pomodoro 2
    
    * pomodoro final
    
    ---------
    
    Co-authored-by: root <root@teamsbot.181.213.132.2>

--- Aprendendo_python_1_hora.py
+++ Aprendendo_python_1_hora.py
@@ -103,3 +103,86 @@ print(lista[0:3])
 print(lista[:4])
 print(lista[-2])
 print(lista)
+
+print(lista[:])
+
+
+numeros = list(range(1, 11) )
+print(numeros, numeros[1: 7: 2])
+
+numeros2 = list(range(1, 32, 2))
+print(numeros2)
+
+## Adição e Remoção
+
+print(dir (numeros))
+
+###
+
+"""
+['__add__', 
+ '__class__', 
+ '__class_getitem__', 
+ '__contains__', 
+ '__delattr__', 
+ '__delitem__', 
+ '__dir__',  
+ '__doc__',
+ '__eq__', 
+ '__format__', 
+ '__ge__', 
+ '__getattribute__', 
+ '__getitem__', 
+ '__gt__', 
+ '__hash__', 
+ '__iadd__', 
+ '__imul__', 
+ '__init__', 
+ '__init_subclass__', 
+ '__iter__', 
+ '__le__', 
+ '__len__', 
+ '__lt__', 
+ '__mul__', 
+ '__ne__', 
+ '__new__', 
+ '__reduce__', 
+ '__reduce_ex__', 
+ '__repr__', 
+ '__reversed__', 
+ '__rmul__', 
+ '__setattr__', 
+ '__setitem__', 
+ '__sizeof__', 
+ '__str__', 
+ '__subclasshook__', 
+ 'append', 
+ 'clear', 
+ 'copy', 
+ 'count', 
+ 'extend', 
+ 'index', 
+ 'insert', 
+ 'pop', 
+ 'remove', 
+ 'reverse', 
+ 'sort']
+"""
+
+###
+
+##print(help(numeros.pop))
+print(numeros.pop())
+print(numeros)
+print(numeros.append(10))
+print(numeros)
+
+### Laços de Repetição
+
+idades = [17, 38, 70]
+print(classificar(idades[0], 'Renzo'))
+
+for idade in idades:
+    print(classificar(idade, 'Pessoa'))
+=======
+
