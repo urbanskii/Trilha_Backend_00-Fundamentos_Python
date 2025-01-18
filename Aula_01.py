eff98d8ffee8c917930780aeb2d2c38cf0613a03
urbanskii <marcos.urbanski@gmail.com>
2025-01-18 12:02:09 +0000

    Realizado os exercicios da Aula_01
    Close #6

---
+++ Aula_01.py
@@ -0,0 +1,18 @@
+#Exercio 01
+
+print('Olá mundo')
+type('Olá mundo')
+
+#Exercicio 02
+
+numero = input('Digite um número: ')
+print(f'O número informado foi {numero}')
+
+
+#Exercicio 03
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+soma = numero_1 + numero_2
+print(f'A soma dos dois números é {soma}')
+
