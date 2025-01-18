0c83f0a2a70c72b5160dcab51a28a9e767886884
urbanskii <marcos.urbanski@gmail.com>
2025-01-18 13:25:39 +0000

    Realizado a aula 03 e exercicios
    Close #9

---
+++ Aula_03.py
@@ -0,0 +1,14 @@
+#Exercicio 03
+
+sexo = input('Digite o seu sexo. (M para Masculino, F para Feminino, I para indefinido): ').upper()
+if sexo  == 'M':
+    print('Masculino')
+elif sexo == 'F':
+    print('Feminino')
+elif sexo == 'I':
+    print('Indefinido') 
+else:
+    print('Sexo Inválido')
+
+
+print('Finalizou o programa')   
