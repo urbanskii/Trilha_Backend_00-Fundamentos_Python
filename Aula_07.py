4831e8ef4e6d63025ba7bfba0db6adc681b2a10a
urbanskii <marcos.urbanski@gmail.com>
2025-01-21 13:32:19 +0000

    realizado o exercicio aula-07
    Close #12

---
+++ Aula_07.py
@@ -0,0 +1,13 @@
+############## Estruturas de Repetição
+
+while True:
+    try:
+        numero = int(input('Digite um valor de 0 a 10: '))
+    except ValueError:
+        print('Deve ser fornecido um valor inteiro')
+    else:
+        if 0 <= numero <= 10:
+            print(f'Número informado é: {numero}')
+            break
+        else:
+            print('O número deve estar entre 0 e 10')
