8601c87d5f6074356688c7e7e872719dc9cd3e98
urbanskii <marcos.urbanski@gmail.com>
2025-01-22 13:43:30 +0000

    realizado a aula 14
    Close #19

---
+++ Aula_14.py
@@ -0,0 +1,9 @@
+salarios = [200, 250, 320, 413, 516, 680, 791, 877, 999, 1000, 2000, 3000]
+contagem_de_faixa_salarial = [0] * 9
+for salario in salarios:
+    indice = salario // 100 -2
+    indice_maximo = len(contagem_de_faixa_salarial) - 1
+    indice = min(indice, indice_maximo)
+    contagem_de_faixa_salarial[indice] += 1
+
+print(contagem_de_faixa_salarial)
