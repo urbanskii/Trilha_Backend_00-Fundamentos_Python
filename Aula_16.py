90711f62d4035471dabdb3f330f9d3664bd2a871
urbanskii <marcos.urbanski@gmail.com>
2025-01-23 12:18:57 +0000

    Realizado a aula 16
    Close #21

--- Aula_16.py
+++ Aula_16.py
@@ -1,7 +1,7 @@
 def imprimir_triangulo_de_numeros_crescentes(n: int):
-    for i in range(1, n + 1):
-        for _ in range(i):
-            print(i, end = '   ')
+    for linha in range(1, n + 1):
+        for coluna in range(1, linha + 1):
+            print(coluna, end = '   ')
         print('')
 
 print('Triangulo com 1 ')
