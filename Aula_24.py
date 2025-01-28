9aa42e608cd34ebf9c892cfef93489b7128ee607
urbanskii <marcos.urbanski@gmail.com>
2025-01-28 12:06:22 +0000

    realizado o exercicio da aula 24

---
+++ Aula_24.py
@@ -0,0 +1,10 @@
+class Quadrado:
+    def __init__(self, lado = 1):
+        self.lado = lado
+
+    def calcular_area(self):
+        return self.lado ** 2
+
+quadrado = Quadrado(4)
+print(quadrado.lado, quadrado.calcular_area())
+
